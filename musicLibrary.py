1e2a4207e920e02d661bf8657da3709f5d44cdb9
nagarajsori <nagarajsori9538@gmail.com>
2025-05-06 02:02:14 +0530

    Initial commit for Jarvis Virtual Assistant

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "love": "https://youtube.com/shorts/_Y1QQ2gQm3o?feature=shared",
+    "kgf": "https://youtube.com/shorts/Tw_Unof1-RE?feature=shared",
+    "vikram": "https://youtube.com/shorts/ZOvAylsM1sQ?feature=shared",
+    "chalisa": "https://youtube.com/shorts/kou3Whs8yZ4?feature=shared"
+}
